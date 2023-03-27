723509cd2932652b9c0209bd7cf36647d3e987f8
oatfazwaz <93711042+oatfazwaz@users.noreply.github.com>
2023-03-27 17:24:11 +0700

    fixed login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -30,6 +30,7 @@ export class AuthService {
     const payload = { username: user.username, sub: user._id };
     return {
       access_token: this.jwtService.sign(payload),
+      user,
     };
   }
 
