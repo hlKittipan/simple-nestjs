5067295d7408f559728d5a66ad06f657a82c97bb
oatfazwaz <93711042+oatfazwaz@users.noreply.github.com>
2023-03-29 16:26:09 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
 
   const appPort = configService.get<number>('PORT', 3000);
