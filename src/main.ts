db599923d28666be50eaffa815483e2daa0a9d56
hlkittipan <hlkittipan@gmail.com>
2023-03-30 09:24:22 +0700

    enable limit post data

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   const configService = app.get(ConfigService);
-
   const appPort = configService.get<number>('PORT', 3000);
   await app.listen(appPort);
 
