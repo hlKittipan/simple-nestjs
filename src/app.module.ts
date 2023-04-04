7f3d0a622302a78e5f0098d8a92f056970d14fcf
hlkittipan <hlkittipan@gmail.com>
2023-04-04 17:06:10 +0700

    remove graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,6 @@ import { LoggerMiddleware } from '@middleware/logger/logger.middleware';
  * External library
  * */
 import { MongooseModule } from '@nestjs/mongoose';
-// import { GraphQLModule } from '@nestjs/graphql';
-// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -23,12 +21,6 @@ import { MongooseModule } from '@nestjs/mongoose';
       load: [configuration],
       cache: true,
     }),
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    //   autoSchemaFile: 'schema.gql',
-    //   debug: process.env.ENV === 'DEV',
-    //   playground: process.env.ENV === 'DEV',
-    // }),
     MongooseModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
